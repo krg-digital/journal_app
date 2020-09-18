0e693bd410d23352264b79fc6fb72d982220e3ec
Kevin Gonzalez <krg@tutamail.com>
2020-09-18 09:39:08 -0700

    prototype using an list for 'entries'

--- app.py
+++ app.py
@@ -8,6 +8,14 @@ welcome = "Welcome to the programming diary!"
 
 print(welcome)
 
+# Sample 'entries' list.
+# entries = [
+#     {"content": "Today I started learning programming.", "date": "01-01-2020"},
+#     {"content": "I created my first SQLite database!", "date": "02-01-2020"},
+#     {"content": "I finished writing my programming diary application.", "date": "03-01-2020"},
+#     {"content": "Today I'm going to continue learning programming!", "date": "04-01-2020"},
+# ]
+
 # ':=' is the walrus operator. 
 while (user_input := input(menu)) != "3":
     if user_input == "1":
