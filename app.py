0936dbf9ae18531ae84a46708810c699127f3f52
Kevin Gonzalez <krg@tutamail.com>
2020-09-18 09:15:12 -0700

    basic welcome menu

--- app.py
+++ app.py
@@ -1,2 +1,18 @@
-'''f
-'''
+menu = """Please select one of the following options:
+1) Add new entry for today.
+2) View entries.
+3) Exit.
+
+Your selection: """
+welcome = "Welcome to the programming diary!"
+
+print(welcome)
+
+# ':=' is the walrus operator. 
+while (user_input := input(menu)) != "3":
+    if user_input == "1":
+        print("Adding...")
+    elif user_input == "2":
+        print("Viewing...")
+    else:
+        print("Invalid option, please try again!")
